6d4209b53cb2272af317d566e7b0d3cf0cffb1ed
yukairex <wyktju@gmail.com>
2020-10-14 07:14:08 -0700

    add eye emoji

--- commands/withdraw.js
+++ commands/withdraw.js
@@ -93,7 +93,7 @@ module.exports = async (msg) => {
     return;
   }
 
-  console.log('processing withdraw to:', address);
+  console.log('👀 processing withdraw to:', address);
   console.log('tx:', result.txHash);
   msg.obj.author.send(
     '🚀 Success! Your TX hash on zkSync is ' +
